3f5ded4c1a5d8fff6f6ebfc24cf4fcd368bfceca
fippolo <filippo.fanesi@gmail.com>
2026-01-13 18:59:53 +0100

    Update Program.cs
    allowed CORS for development porpuses

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -3,8 +3,19 @@ using GestioneOrdiniRistorante.Web.Extension;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//CORS necessary for development nodejs server, critical to remove at prod time
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("SpaCors", policy =>
+    {
+        policy
+        .WithOrigins("http://localhost:4200")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
+    });
+});
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -14,8 +25,9 @@ builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddWebServices(builder.Configuration);
 builder.Services.AddOptions(builder.Configuration);
 
-var app = builder.Build();
 
+var app = builder.Build();
+app.UseCors("SpaCors");
 app.AddWebMiddleware();
 
 
