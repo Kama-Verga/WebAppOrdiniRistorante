34160d4d330c6801efc6d07a25c30779939fcf91
Kama-Verga <vergariandrea984@gmail.com>
2026-01-24 20:10:10 +0100

    aggiustamenti responsabilità

--- GestioneOrdiniRistorante.Web/Service/Interface/ServiceOrdineInt.cs
+++ GestioneOrdiniRistorante.Web/Service/Interface/ServiceOrdineInt.cs
@@ -1,5 +1,6 @@
 ﻿using GestioneOrdiniRistorante.Models;
 using GestioneOrdiniRistorante.Models.Models.DTO;
+using GestioneOrdiniRistorante.Models.Models.Request;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,7 @@ namespace GestioneOrdiniRistorante.Application.Service.Interface
 {
     public interface ServiceOrdineInt
     {
-        Task<Ordine> CreaOrdine(Ordine a);
+        Task<Ordine> CreaOrdine(CreaOrdineReq req, int userId);
         Task<VisualizzaOrdineDTO> TrovaOrdine(int numeroOrdine);
         Task<List<Ordine>> TrovaOrdiniConUtente(DateTime Inizio, DateTime Fine, int a);
         Task<List<Ordine>> TrovaOrdini(DateTime Inizio, DateTime Fine);
--- GestioneOrdiniRistorante.Web/Service/ServiceOrdine.cs
+++ GestioneOrdiniRistorante.Web/Service/ServiceOrdine.cs
@@ -4,6 +4,7 @@ using GestioneOrdiniRistorante.Infrastructure.Repositories.Abstractions;
 using GestioneOrdiniRistorante.Models;
 using GestioneOrdiniRistorante.Models.Entities;
 using GestioneOrdiniRistorante.Models.Models.DTO;
+using GestioneOrdiniRistorante.Models.Models.Request;
 using System.ComponentModel.Design;
 
 namespace GestioneOrdiniRistorante.Service
@@ -12,20 +13,42 @@ namespace GestioneOrdiniRistorante.Service
     {
 
         private readonly OrdineRepo OrdineDB;
+        private readonly ServiceProdottoInt ProdottoS;
         private readonly ProdottoInOrdineRepo ProdottoInOrdineDB;
 
-        public ServiceOrdine(OrdineRepo or, ProdottoInOrdineRepo pio)
+        public ServiceOrdine(OrdineRepo or, ProdottoInOrdineRepo pio, ServiceProdottoInt IntTemp2)
         {
             OrdineDB = or;
             ProdottoInOrdineDB = pio;
+            ProdottoS = IntTemp2;
         }
-        public async Task<Ordine> CreaOrdine(Ordine ordine)
+        public async Task<Ordine> CreaOrdine(CreaOrdineReq req, int userId)
         {
-            OrdineDB.AddAsync(ordine); // Aggiunge l'ordine
-            await OrdineDB.SalvaDatiAsync(); // Salva solo una volta al termine
+            Ordine ordine = req.ToEntity(userId);
+
+            List<Prodotto> prodotti = new List<Prodotto>();
+
+            foreach (int prodottoId in req.Contenuto)
+            {
+                Prodotto prodotto = await ProdottoS.TrovaProdotto(prodottoId);
+
+                // Se TrovaProdotto può tornare null, qui va gestito (dimmi come vuoi gestirlo: 404? 400?)
+                // if (prodotto == null) throw ...
+
+                prodotti.Add(prodotto);
+                ordine.AggiungiProdotto(prodotto);
+            }
+
+            ordine.Prezzo = CalcolaTotaleConSconto(prodotti);
+
+            // SALVATAGGIO: identico al tuo service
+            OrdineDB.AddAsync(ordine);
+            await OrdineDB.SalvaDatiAsync();
+
             Console.WriteLine(ordine.UtenteId);
             return ordine;
         }
+
 
         // Trova ordine + lista id prodotti (DTO)
         public async Task<VisualizzaOrdineDTO> TrovaOrdine(int numeroOrdine)
@@ -61,5 +84,30 @@ namespace GestioneOrdiniRistorante.Service
             Console.WriteLine("fatto");
             return T;
         }
+
+
+        public static decimal CalcolaTotaleConSconto(List<Prodotto> prodotti)
+        {
+            decimal totale = 0;
+
+            // Raggruppiamo i prodotti per tipo
+            var gruppi = prodotti.GroupBy(p => p.Tipo);
+
+            // Per ogni gruppo applicare lo sconto del 10%
+            foreach (var gruppo in gruppi)
+            {
+                // Calcoliamo la somma del gruppo
+                decimal sommaGruppo = gruppo.Sum(p => p.Prezzo);
+
+                // Applichiamo lo sconto del 10% al gruppo
+                decimal sommaScontata = sommaGruppo * 0.9m;
+
+                // Aggiungiamo al totale
+                totale += sommaScontata;
+            }
+
+            return totale;
+        }
+
     }
 }
